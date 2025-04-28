b252cc35e978fd358c3af9fab29f7e4b4197e8cc
MandyE69 <mandipgyawali436@gmail.com>
2025-04-28 21:37:26 +0545

    connected supabase in the project and created database folder

--- backend/app.js
+++ backend/app.js
@@ -1,66 +1,54 @@
-const express = require("express");
-const app = express();
+  const express = require("express");
+  const app = express();
+  require("./database/connection")
 
-// let app = require("express")();
+  // let app = require("express")();
 
-// app.get("/",(req,res)=>{ //(request,response) /// yo chai api bhayoo jun ma json send gareko chau
-//     res.json({
-//       name : "madhav",
-//       address : "Chitwan"
-//     })
-// })
+  // app.get("/",(req,res)=>{ //(request,response) /// yo chai api bhayoo jun ma json send gareko chau
+  //     res.json({
+  //       name : "madhav",
+  //       address : "Chitwan"
+  //     })
+  // })
 
+  // app.get("/about",(req,res)=>{ // yo chai api hoina route bhanna milyo or route nai bhanyoo
+  //     res.send("This is about page")
+  // })
 
-// app.get("/about",(req,res)=>{ // yo chai api hoina route bhanna milyo or route nai bhanyoo
-//     res.send("This is about page")
-// })
+  // app.post("/register", (req,res)=>{
+  //   res.json({
+  //     name :"Registeration confirmed"
+  //   })
+  // })
 
-// app.post("/register", (req,res)=>{
-//   res.json({
-//     name :"Registeration confirmed"
-//   })
-// })
+  app.get("/books", (req, res) => {
+    // logic to fetch data from database
+    res.json({
+      message: "read books successfully",
+    });
+  });
 
-app.get("/books", (req,res)=>{
-  // logic to fetch data from database
-  res.json({
-    message: "read books successfully"
-  })
-})
+  app.post("/books", (req, res) => {
+    // logic to create data
+    res.json({
+      message: "create books successfully",
+    });
+  });
 
-app.post("/books", (req,res)=>{
-// logic to create data 
-  res.json({
-    message: "create books successfully"
-  })
-})
+  app.delete("/books/:id", (req, res) => {
+    // logic to delete data
+    res.json({
+      message: "deleted books successfully",
+    });
+  });
 
-app.delete("/books/:id", (req,res)=>{
-// logic to delete data
-  res.json({
-    message: "deleted books successfully"
-  })
-})
+  app.patch("/books/:id", (req, res) => {
+    // logic to update data
+    res.json({
+      message: "update books successfully",
+    });
+  });
 
-app.patch("/books/:id", (req,res)=>{
-  // logic to update data
-  res.json({
-    message: "update books successfully"
-  })
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000,function(){
-  console.log("server/backend/node project has started at port 3000")
-})
+  app.listen(3000, function () {
+    console.log("server/backend/node project has started at port 3000");
+  });
